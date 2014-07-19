6c35a53e7bb8fbdc8c054ddfb8153ac0793e2efb
Tony DiCola <tony@tonydicola.com>
2014-07-19 02:26:25 +0000

    Fix bug with PWM choosing pin number for Raspberry Pi.

--- Adafruit_GPIO/PWM.py
+++ Adafruit_GPIO/PWM.py
@@ -46,7 +46,7 @@ class RPi_PWM_Adapter(object):
 		if dutycycle < 0.0 or dutycycle > 100.0:
 			raise ValueError('Invalid duty cycle value, must be between 0.0 to 100.0 (inclusive).')
 		# Make pin an output.
-		self.rpi_gpio.setup(12, self.rpi_gpio.OUT)
+		self.rpi_gpio.setup(pin, self.rpi_gpio.OUT)
 		# Create PWM instance and save a reference for later access.
 		self.pwm[pin] = self.rpi_gpio.PWM(pin, frequency_hz)
 		# Start the PWM at the specified duty cycle.
