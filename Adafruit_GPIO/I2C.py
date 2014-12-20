fc85902aa951c9c0388b3d390c520b43a8e833f4
Tony DiCola <tony@tonydicola.com>
2014-12-20 15:47:11 -0800

    Update default busnum parameter value to allow importing Adafruit_GPIO.I2C on unsupported platforms for testing.

--- Adafruit_GPIO/I2C.py
+++ Adafruit_GPIO/I2C.py
@@ -56,11 +56,13 @@ def get_default_bus():
     else:
         raise RuntimeError('Could not determine default I2C bus for platform.')
 
-def get_i2c_device(address, busnum=get_default_bus(), **kwargs):
+def get_i2c_device(address, busnum=None, **kwargs):
     """Return an I2C device for the specified address and on the specified bus.
     If busnum isn't specified, the default I2C bus for the platform will attempt
     to be detected.
     """
+    if busnum is None:
+        busnum = get_default_bus()
     return Device(address, busnum, **kwargs)
 
 
